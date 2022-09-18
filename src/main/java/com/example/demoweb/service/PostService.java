04a1bd45e3648a709be4e224da30e5073c3f4031
pryseolg <oleg.kuts.02@mail.ru>
2022-09-18 22:56:56 +0500

    Added posts is worked (7.5)

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -14,12 +14,12 @@ public class PostService {
 
     public PostService() {
         this.posts = new ArrayList<>();
-        addPost("Never Gonna Give You Up, never gonna let you down\n", new Date());
-        addPost("Never gonna run around and desert you\n", new Date());
-        addPost("Never gonna make you cry, never gonna say goodbye", new Date());
+        create("Never Gonna Give You Up, never gonna let you down\n", new Date());
+        create("Never gonna run around and desert you\n", new Date());
+        create("Never gonna make you cry, never gonna say goodbye", new Date());
     }
 
-    public void addPost(String text, Date creationDate) {
+    public void create(String text, Date creationDate) {
         posts.add(new Post(text, creationDate));
     }
 }
